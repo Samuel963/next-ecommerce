3ccc6ddd70dd55a08001158acff5ac314370578f
samuel assis <samuelassis@MacBook-Pro-de-samuel.local>
2025-02-16 17:09:27 -0300

    change base url

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,6 +1,6 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const BASE_URL = 'http://localhost:3001/api/v1/'
+export const BASE_URL = 'https://e-commerce-back-production-c91f.up.railway.app/api/v1/'
 
 export const server = dev
   ? "http://localhost:3001/api/v1/"
