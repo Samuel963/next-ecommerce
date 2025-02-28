ee5450ee3d410cac1928f2c2e5eae61ddcce7aac
Samuel Assis <samuelassis@MacBook-Pro-de-Samuel.local>
2025-02-28 17:06:15 -0300

    add admin pages

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,6 +1,6 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const BASE_URL = !dev ? 'http://localhost:3001/api/v1/' : 'https://rayssacortealaser.up.railway.app/api/v1/';
+export const BASE_URL = dev ? 'http://localhost:3001/api/v1/' : 'https://rayssacortealaser.up.railway.app/api/v1/';
 
 export const server = dev
   ? "http://localhost:3001/api/v1/"
